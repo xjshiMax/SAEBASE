3e54ceebb3aa2e2fbb21cc333f97ed8382f692f5
xjshi <xjshi@coremail.cn>
2018-12-17 18:02:47 +0800

    SAEBASE  简易基础库工程提交。 12/17 tcpserver windows上测试没问题。

---
+++ TCPServertest/TCPServertest.cpp
@@ -0,0 +1,34 @@
+//≤‚ ‘≥Ã–Ú£¨
+//≤‚ ‘tcpserver reactor select 
+
+#include "../base/include/xTcpServerBase.hpp"
+using namespace SAEBASE;
+
+class mytcpsvr:public xTcpServerBase
+{
+	virtual int Onaccept(int socketfd,char*date,int len,IN xEventHandler *clientHandle=NULL)
+	{
+		printf(" in Onaccept!\n");
+		return 0;
+	}
+	virtual int Ondata(int socketfd,char*date,int len)
+	{
+		printf("in Ondata!\n");
+		return 0;
+	}
+	virtual int Onclose(int socketfd)
+	{
+		printf("in Onclose!\n");
+		return 0;
+	}
+};
+
+int main(int argc, char* argv[])
+{
+	mytcpsvr m_tcpsvr;
+	xReactor m_reactor;
+	m_tcpsvr.startTCPServer(&m_reactor,"192.168.200.80",6000);
+	m_reactor.start();
+	return 0;
+}
+
